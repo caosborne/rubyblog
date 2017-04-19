7c104ae88f3faaa6fd1d8b737b267d4fd9b3041b
Christopher Osborne <chris.a.osborne@gmail.com>
2017-04-19 12:36:14 -0500

    Update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    # debugger
     @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
@@ -52,7 +51,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
